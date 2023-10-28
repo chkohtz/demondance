efa6f8d6a74437fff1ee4fd99df35dfb5ec60800
chkohtz <84747967+chkohtz@users.noreply.github.com>
2023-10-28 02:26:11 -0400

    player animations

--- dancedancedamnation/Assets/PlayerAnimator.cs
+++ dancedancedamnation/Assets/PlayerAnimator.cs
@@ -5,15 +5,41 @@ using UnityEngine;
 public class PlayerAnimator : MonoBehaviour
 {
 
-    Animator player;
+    public Animator player;
+    public bool playerHurt;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Here for testing purposes
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player.SetTrigger("Hurt");
+            return;
+        }
+
+        //trigger anim for each direction
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            player.SetTrigger("up");
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            player.SetTrigger("down");
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            player.SetTrigger("left");
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            player.SetTrigger("right");
+        }
     }
 }
+
